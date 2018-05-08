3e61175451c0d1fe79c922000b85d173c3d837df
Sergio R <srg00017@gmail.com>
2018-05-08 17:00:56 +0200

    Added some extra comments, and better handling for input string.

--- src/scala/org/stratio/challenge/PhoneBillCalculator.scala
+++ src/scala/org/stratio/challenge/PhoneBillCalculator.scala
@@ -3,17 +3,26 @@ package scala.org.stratio.challenge
 /**
  * @author Sergio Ramírez
  */
-object Stratio extends App {
+object PhoneBillCalculator extends App {
   
-  
-  def aggregateTime(prev: Int, next: Int) = prev * 60 + next
+  private def aggregateTime(prev: Int, next: Int) = prev * 60 + next
   
   def computePhoneBill(callLog: String) = {
     
-    /** Format calls, from string to PhoneCall objects **/
+    /** Check pre-requisites **/
     val rawCalls = callLog.split("\n")
+    // Empty or lengthy log
+    if(callLog.isEmpty() || rawCalls.size > 100) 
+      throw new IllegalArgumentException("Number of different lines in S must be [0,100]")
+    // Regular expresion for format: hh:mm:ss,nnn-nnn-nnn
+    val regex = "^(?:([01]?\\d|2[0-3]):([0-5]?\\d):)?([0-5]?\\d),(?:\\d{3}-){2}\\d{3}$"
+    rawCalls.zipWithIndex.foreach{ case(s, index) =>
+      if(!s.matches(regex))
+        throw new IllegalArgumentException("Line %d does not follow the format: hh:mm:ss,nnn-nnn-nnn".format(index))
+    }    
+    
+    /** Format calls, from string to PhoneCall objects **/
     val calls = rawCalls.map{ s =>
-      
       val parts = s.split(',')
       val call = parts.size match {
         case 2  => 
@@ -27,15 +36,15 @@ object Stratio extends App {
     }
     println("Number of phone calls: " + calls.size)
     
-    /** Find the most-frequent phone number in the log (longest total duration and smallest phone number) **/
+    /** Find the most-frequent phone number in the log (longest total duration and smallest phone value) **/
     val mumNumber = calls
         .groupBy(_.phoneNumber)
         .mapValues(_.map(_.duration).sum)
         .map{ case (number, duration) => new PhoneCall(duration, number) }
-        .max // maximum total duration, if tie smallest number
+        .max // maximum total duration, if tie, smallest number
     println("Phone number with free promotion: " + mumNumber.phoneNumber)
 
-    /** Compute total cost for each phone call, always applying correspondent discounts and tariffs **/
+    /** Compute total cost for each phone call, always applying associated discounts and tariffs **/
     val total = calls.map { c =>
       c.phoneNumber match {
         case l if l == mumNumber.phoneNumber | l == Long.MinValue => 0
@@ -43,7 +52,8 @@ object Stratio extends App {
       }
       
     }.sum
-    println("Total: " + total)
+    
+    println("Total payment: " + total)
     total
   }
 }
